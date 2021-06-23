85225d79cc367f42e1d08ab13d1dd629ca2d9e11
ezratturner <82107035+ezratturner@users.noreply.github.com>
2021-06-23 10:14:39 +0100

    workflows

---
+++ terraform/main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "2.46.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "main" {
+  # creating a rg name here
+  name     = "ett-tf-rg"
+  location = "uksouth"
+
+}
